c92c2fa6cac5d7252d82bb0ec86e3e4709f6f934
dhmjulien <dhmjulien@gmail.com>
2019-03-05 16:16:26 +0100

    Added fecth from img folder feature + fixes

--- js/galerie.js
+++ js/galerie.js
@@ -1,28 +1,61 @@
-let images = ["bateau.jpg", "batiment.jpg", "carroussel.jpg", "chevaux.jpg", "dragon.jpg", "ile_de_nantes.jpg", "roue.jpg", "spider.jpg", "tulipe.jpg"];
+// let images = ['bateau.jpg', 'batiment.jpg', 'carroussel.jpg', 'chevaux.jpg', 'dragon.jpg', 'ile_de_nantes.jpg', 'roue.jpg', 'spider.jpg', 'tulipe.jpg'];
 
-let modal = document.getElementById("modal");
-let photos = document.getElementById("photos");
+let images = [];
+
+let modal = document.getElementById('modal');
+let photos = document.getElementById('photos');
 let img = document.getElementById('myImg');
-let modalImg = document.getElementById("img01");
-let span = document.getElementsByClassName("close")[0];
+let modalImg = document.getElementById('img01');
+let span = document.getElementsByClassName('close')[0];
+
+
+// FETCH //
+
+fetch('../img/')
+    .then(res => {
+        return res.text();
+    })
+    .then(html => {
+        let parser = new DOMParser();
+        let doc = parser.parseFromString(html, 'text/html');
+        let a = doc.querySelectorAll('a');
+        a.forEach(image => {
+            if (image.href.includes('.jpg') || image.href.includes('.png')) {
+                let linkArray = image.href.split('/');
+                let link = linkArray[(linkArray.length) - 1];
+                images.push(link);
+            };
+        });
+    })
+    .catch(err => {
+        console.log('fetch error', err);
+    });
+
+console.log(images);
+
+
+// GENERATES GALLERY //
 
 images.forEach(image => {
-    let div = document.createElement("div");
-    div.classList += "img-wrap mx-1 my-1";
-    let card = `<img class="img-fluid photo" id="${image}" src="../img/${image}">`;
-    div.innerHTML = card;
+    let div = document.createElement('div');
+    div.classList += 'img-wrap mx-2 my-2';
+    let img = `<img class='img-fluid photo' id='${image}' src='../img/${image}'>`;
+    div.innerHTML = img;
     photos.appendChild(div);
 });
 
-let galerie = document.querySelectorAll(".photo");
+
+//  CREATES MODAL //
+
+let galerie = document.querySelectorAll('.photo');
 
 galerie.forEach(image => {
-    image.addEventListener("click", () => {
-        modal.style.display = "block";
+    image.addEventListener('click', () => {
+        modal.style.display = 'block';
         modalImg.src = `../img/${image.id}`;
     });
 });
 
-span.addEventListener("click", () => {
-    modal.style.display = "none";
+span.addEventListener('click', () => {
+    modal.style.display = 'none';
 });
