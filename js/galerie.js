51ebd50c2c3974587bc03c4531ae9035af6265a7
dhmjulien <dhmjulien@gmail.com>
2019-03-05 13:42:31 +0100

    Add modal feature

--- js/galerie.js
+++ js/galerie.js
@@ -1,11 +1,28 @@
 let images = ["bateau.jpg", "batiment.jpg", "carroussel.jpg", "chevaux.jpg", "dragon.jpg", "ile_de_nantes.jpg", "roue.jpg", "spider.jpg", "tulipe.jpg"];
 
+let modal = document.getElementById("modal");
 let photos = document.getElementById("photos");
+let img = document.getElementById('myImg');
+let modalImg = document.getElementById("img01");
+let span = document.getElementsByClassName("close")[0];
 
-images.forEach((image) => {
+images.forEach(image => {
     let div = document.createElement("div");
     div.classList += "img-wrap mx-1 my-1";
-    let card = `<img class="img-fluid photo" src="../img/${image}">`;
+    let card = `<img class="img-fluid photo" id="${image}" src="../img/${image}">`;
     div.innerHTML = card;
     photos.appendChild(div);
+});
+
+let galerie = document.querySelectorAll(".photo");
+
+galerie.forEach(image => {
+    image.addEventListener("click", () => {
+        modal.style.display = "block";
+        modalImg.src = `../img/${image.id}`;
+    });
+});
+
+span.addEventListener("click", () => {
+    modal.style.display = "none";
 });
